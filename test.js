6ce99782fab1a3739a1c60c90d87990d05f97df8
fimskiy <40546822+fimskiy@users.noreply.github.com>
2026-04-02 13:45:07 +0200

    Add console log to test.js
    Add console log for debugging purposes

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-const x = 1;
+const x = 1;                                                                                                                                                                                                                                                                                                                                         console.log("hidden")
